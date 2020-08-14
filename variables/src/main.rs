821d0a23074165bf9d56057d7bfd9759434cc0ef
Aditya <adityakr082@gmail.com>
2020-08-14 09:34:17 +0530

    initial commit

---
+++ variables/src/main.rs
@@ -0,0 +1,13 @@
+fn main()
+{
+    let a = [1, 2, 3, 4, 5];
+
+    let first = a[0];
+    let second = a[1];
+
+    let index = 10;
+    
+    let element = a[index];
+
+    println!("The value of element is: {}", index);
+}
